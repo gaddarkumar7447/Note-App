2e34a6c0022c316e4611717f4a5df9faff518a00
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2022-08-15 10:17:13 +0530

    Fetch data from database

---
+++ app/src/main/java/com/example/noteapp/FireBaseModel.java
@@ -0,0 +1,27 @@
+package com.example.noteapp;
+
+public class FireBaseModel {
+        private String title;
+        private String content;
+
+    public FireBaseModel(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- app/src/main/java/com/example/noteapp/Notes.java
+++ app/src/main/java/com/example/noteapp/Notes.java
@@ -2,6 +2,8 @@ package com.example.noteapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,18 +12,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Notes extends AppCompatActivity {
     private FloatingActionButton mfloatingActionButton;
     private FirebaseAuth firebaseAuth;
+    RecyclerView mrecyclerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseRecyclerAdapter<FireBaseModel, NoteViewHolder> noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        getSupportActionBar().setTitle("All Notes");
         firebaseAuth = FirebaseAuth.getInstance();
-        getSupportActionBar().hide();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
         mfloatingActionButton = findViewById(R.id.createNoteFab);
         mfloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +57,12 @@ public class Notes extends AppCompatActivity {
                 firebaseAuth.signOut();
                 finish();
                 startActivity(new Intent(Notes.this, MainActivity.class));
+                break;
 
+            case R.id.signUpG:
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(Notes.this, SignUp.class));
         }
         return super.onOptionsItemSelected(item);
     }
